0cf6602d37f61194782c687babbb5a20b7b4c322
hendisantika <hendisantika@yahoo.co.id>
2024-10-10 11:00:43 +0700

    Add retryWithCustomConfig in RetryConfiguration🫸🌀✏️📗🐧🐳⬆

--- src/main/java/id/my/hendisantika/resilience4jretry/config/RetryConfiguration.java
+++ src/main/java/id/my/hendisantika/resilience4jretry/config/RetryConfiguration.java
@@ -1,8 +1,16 @@
 package id.my.hendisantika.resilience4jretry.config;
 
+import id.my.hendisantika.resilience4jretry.exception.MovieNotFoundException;
+import io.github.resilience4j.retry.Retry;
+import io.github.resilience4j.retry.RetryConfig;
 import io.github.resilience4j.retry.RetryRegistry;
 import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.HttpClientErrorException;
+import org.springframework.web.client.HttpServerErrorException;
+
+import java.time.Duration;
 
 /**
  * Created by IntelliJ IDEA.
@@ -19,4 +27,16 @@ import org.springframework.context.annotation.Configuration;
 public class RetryConfiguration {
 
     private final RetryRegistry retryRegistry;
+
+    @Bean
+    public Retry retryWithCustomConfig() {
+        RetryConfig customConfig = RetryConfig.custom()
+                .maxAttempts(3)
+                .waitDuration(Duration.ofSeconds(2))
+                .retryExceptions(HttpClientErrorException.class, HttpServerErrorException.class)
+                .ignoreExceptions(MovieNotFoundException.class)
+                .build();
+
+        return retryRegistry.retry("customRetryConfig", customConfig);
+    }
 }
