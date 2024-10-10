354de0dfd10a072b2fb27997dc954b7cbeaba448
hendisantika <hendisantika@yahoo.co.id>
2024-10-10 10:50:45 +0700

    Create Spring Boot Project 🫸🌀✏️📗🐧🐳⬆

---
+++ src/main/java/id/my/hendisantika/resilience4jretry/SpringBootResilience4jRetryApplication.java
@@ -0,0 +1,13 @@
+package id.my.hendisantika.resilience4jretry;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootResilience4jRetryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootResilience4jRetryApplication.class, args);
+    }
+
+}
---
+++ src/test/java/id/my/hendisantika/resilience4jretry/SpringBootResilience4jRetryApplicationTests.java
@@ -0,0 +1,13 @@
+package id.my.hendisantika.resilience4jretry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootResilience4jRetryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
